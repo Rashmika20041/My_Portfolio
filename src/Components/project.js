c4aac9ee3ce02470645a9f96e01a80182b4396d5
Rashmika Perera <144840582+Rashmika20041@users.noreply.github.com>
2025-10-30 20:54:13 +0530

    fix: normalize finance image filename casing and update import for case-sensitive builds

--- src/Components/project.js
+++ src/Components/project.js
@@ -1,7 +1,7 @@
 import dailybloom from "./assets/dailybloom.png";
 import movieSite from "./assets/movieSite.png";
 import pharmacy from "./assets/pharmacy.png";
-import finance from "./assets/Finance.png";
+import finance from "./assets/finance.png";
 
 const project = [
   {
