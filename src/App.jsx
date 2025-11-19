0c218ae00c710d89a7699bc7a389ab64bcd8cd41
Rashmika Perera <144840582+Rashmika20041@users.noreply.github.com>
2025-11-20 00:22:50 +0530

    Add Vercel Speed Insights integration
    Installed @vercel/speed-insights and added the SpeedInsights component to App.jsx to enable performance monitoring.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Dashboard from './Components/dashboard'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
 
   return (
     <>
       <Dashboard />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
